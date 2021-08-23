b897145cd785fdb1f8c34c67bd4cd398c1b14fb2
nurbakg2023 <nurba.kg2023@gmail.com>
2021-08-24 00:41:42 +0600

    Password Reset

--- nurcrm/urls.py
+++ nurcrm/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
+from django.contrib.auth.views import (LoginView, LogoutView, PasswordResetView,
+                                       PasswordResetDoneView, PasswordResetConfirmView,
+                                       PasswordResetCompleteView, )
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView, SignupView
 
@@ -11,6 +13,10 @@ urlpatterns = [
     path('leads/', include('leads.urls', namespace='leads')),
     path('agents/', include('agents.urls', namespace='agents')),
     path('login/', LoginView.as_view(), name='login'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
